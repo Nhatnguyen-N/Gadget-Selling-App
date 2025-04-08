668c92fcdc1c510da55db9e2e48fdc5c7e3dbf8c
Nhatnguyen-N <nhatnguyen13213@gmail.com>
2025-04-08 20:44:14 +0700

    create project, fix bug expo-router, fontend:home, productDetail, zustand

---
+++ frontend_v2/src/app/(shop)/index.tsx
@@ -0,0 +1,34 @@
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import React from "react";
+import { PRODUCTS } from "@/assets/products";
+import { ProductListItem } from "../../components/product-list-item";
+import { ListHeader } from "@/src/components/list-header";
+type Props = {};
+
+const Home = (props: Props) => {
+  return (
+    <View>
+      <FlatList
+        data={PRODUCTS}
+        renderItem={({ item, index }) => <ProductListItem product={item} />}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+        ListHeaderComponent={ListHeader}
+        contentContainerStyle={styles.flatListContent}
+        columnWrapperStyle={styles.flatListColumn}
+        style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+      />
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  flatListContent: {
+    paddingBottom: 20,
+  },
+  flatListColumn: {
+    justifyContent: "space-between",
+  },
+});
